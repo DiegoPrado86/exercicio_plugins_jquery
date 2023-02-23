daf67549d87f7c6a1296e812c2e0038d038a0372
Diego Prado <diego_prado1986@outlook.com>
2023-02-23 17:26:45 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    $('#cpf').mask('000.000.000-00',{
+        placeholder: '000.000.000-00'
+    }); 
+    $('#cep').mask('00000-000',{
+        placeholder: '00000-000'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            telefone: {
+                required: true 
+            },
+            email: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            } 
+        },
+        messages: {
+            nome: 'Por favor, digite seu nome',
+            telefone: 'Por favor, digite seu telefone',
+            email: 'Por favor, digite seu e-mail',
+            cpf: 'Por favor, digite seu cpf',
+            endereco: 'Por favor, digite seu endereço',
+            cep: 'Por favor, digite seu cep'
+        }
+    })
+});
